20fb6d38cbb6b4105e4f465dbf210b3a622dd67d
Jacob Dufault <jacobdufault@gmail.com>
2013-11-03 13:11:35 -0500

    Cleanup JsonException

--- LitJSON/JsonException.cs
+++ LitJSON/JsonException.cs
@@ -17,26 +17,20 @@ namespace LitJson {
         }
 
         internal JsonException(ParserToken token) :
-            base(String.Format(
-                   "Invalid token '{0}' in input string", token)) {
+            base(String.Format("Invalid token '{0}' in input string", token)) {
         }
 
         internal JsonException(ParserToken token,
                                 Exception inner_exception) :
-            base(String.Format(
-                    "Invalid token '{0}' in input string", token),
-                inner_exception) {
+            base(String.Format("Invalid token '{0}' in input string", token), inner_exception) {
         }
 
         internal JsonException(int c) :
-            base(String.Format(
-                   "Invalid character '{0}' in input string", (char)c)) {
+            base(String.Format("Invalid character '{0}' in input string", (char)c)) {
         }
 
         internal JsonException(int c, Exception inner_exception) :
-            base(String.Format(
-                   "Invalid character '{0}' in input string", (char)c),
-               inner_exception) {
+            base(String.Format("Invalid character '{0}' in input string", (char)c), inner_exception) {
         }
 
         public JsonException(string message)
