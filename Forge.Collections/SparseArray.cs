aa5a2b64ebc8539f118e0d0dc749cadccbd55fcd
Jacob Dufault <jacobdufault@gmail.com>
2013-12-30 21:16:30 -0600

    Make SparseArray.TryGetValue public

--- Forge.Collections/SparseArray.cs
+++ Forge.Collections/SparseArray.cs
@@ -116,6 +116,21 @@ namespace Forge.Collections {
         }
 
         /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key whose value to get.</param>
+        /// <param name="value">When this method returns, the value associated with the specified
+        /// key, if the key is found; otherwise, the default value for the type of the
+        /// <paramref name="value" />parameter. This parameter is passed uninitialized.</param>
+        /// <returns>true if the object that implements
+        /// <see cref="T:System.Collections.Generic.IDictionary`2" /> contains an element with the
+        /// specified key; otherwise, false.</returns>
+        public bool TryGetValue(int key, out T value) {
+            value = this[key];
+            return Contains(key);
+        }
+
+        /// <summary>
         /// Ensures that index is a valid index into the Elements array.
         /// </summary>
         /// <param name="index">The index to verify.</param>
@@ -143,11 +158,6 @@ namespace Forge.Collections {
                 return (from pair in this
                         select pair.Key).ToList();
             }
-        }
-
-        bool IDictionary<int, T>.TryGetValue(int key, out T value) {
-            value = this[key];
-            return Contains(key);
         }
 
         ICollection<T> IDictionary<int, T>.Values {
